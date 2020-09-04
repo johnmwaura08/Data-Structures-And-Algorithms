b7b5a2e7bfb544217c5f443bed24347172031291
John Mwaura <johnmwaura08@gmail.com>
2020-09-04 12:35:14 -0500

    introduced math.floor

--- BASICS/randomValues.js
+++ BASICS/randomValues.js
@@ -12,8 +12,10 @@ so we can return the value of the Math.random() function.
 function randomFraction() {
 
   
-  
-    return Math.random()
+  //lets multiply the math.random by 10 to get a number between 0 & 9
+  //we can also use Math.floor to round it up
+
+    return Math.floor( Math.random() * 10 );
 
   }
 
